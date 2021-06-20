75e6e01f3762a4edc80c04480c489d2e6e46f17e
Matheus Santos <mat.ga.santos@hotmail.com>
2021-06-20 17:28:16 -0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,6 +18,6 @@ export class ProductService {
   }
 
   public getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response) => <Product[]> response.json());
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
